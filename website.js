6e5a8571592bc173426801d9bc6a39b61a219e63
staceyle2001 <staceyle2001@gmail.com>
2023-11-24 23:39:30 -0600

    Added homePage.js 11/24/23

--- website.js
+++ website.js
@@ -11,54 +11,6 @@ const mobileMenu = () =>
 
 menu.addEventListener('click', mobileMenu);
 
-// Show active menu when scrolling
-const highlightMenu = () => 
-{
-  const elem = document.querySelector('.highlight');
-  const portfolioMenu = document.querySelector('#portfolio-page');
-  const aboutMenu = document.querySelector('#about-page');
-  const servicesMenu = document.querySelector('#services-page');
-  const contactMenu = document.querySelector('#contact-page');
-  let scrollPos = window.scrollY;
-  // console.log(scrollPos);
-
-  // adds 'highlight' class to my menu items
-  if (window.innerWidth > 960 && scrollPos < 600) 
-  {
-    portfolioMenu.classList.add('highlight');
-    aboutMenu.classList.remove('highlight');
-    return;
-  } 
-  else if (window.innerWidth > 960 && scrollPos < 1400) 
-  {
-    aboutMenu.classList.add('highlight');
-    portfolioMenu.classList.remove('highlight');
-    servicesMenu.classList.remove('highlight');
-    return;
-  } 
-  else if (window.innerWidth > 960 && scrollPos < 2345) 
-  {
-    servicesMenu.classList.add('highlight');
-    aboutMenu.classList.remove('highlight');
-    contactMenu.classList.remove('highlight');
-    return;
-  }
-  else if (window.innerWidth > 960 && scrollPos < 3290) 
-  {
-    contactMenu.classList.add('highlight');
-    servicesMenu.classList.remove('highlight');
-    return;
-  }
-
-  if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) 
-  {
-    elem.classList.remove('highlight');
-  }
-};
-
-window.addEventListener('scroll', highlightMenu);
-window.addEventListener('click', highlightMenu);
-
 //  Close mobile Menu when clicking on a menu item
 const hideMobileMenu = () => 
 {
@@ -72,27 +24,6 @@ const hideMobileMenu = () =>
 
 menuLinks.addEventListener('click', hideMobileMenu);
 navLogo.addEventListener('click', hideMobileMenu);
-
-// Get a reference to the header element
-const header = document.querySelector('.welcome__heading');
-
-// Define a scroll event listener
-window.addEventListener('scroll', () => {
-  // Determine the scroll position
-  const scrollY = window.scrollY;
-
-  // Define the scroll position at which you want the header to expand
-  const scrollThreshold = 50; // Change this value as needed
-
-  // Check if the user has scrolled past the threshold
-  if (scrollY > scrollThreshold) {
-    // Add the "expanded" class to the header element
-    header.classList.add('expanded');
-  } else {
-    // Remove the "expanded" class if the user scrolls back up
-    header.classList.remove('expanded');
-  }
-});
 
 // Back to top button 
 // Get a reference to the button element
@@ -123,4 +54,3 @@ backToTopButton.addEventListener('click', () =>
         behavior: 'smooth' // Smooth scrolling animation
     });
 });
-
