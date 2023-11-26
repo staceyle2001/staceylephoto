3475f7d4259702b12c260e240bf41dcfeb8c935c
staceyle2001 <staceyle2001@gmail.com>
2023-11-26 12:37:58 -0600

    Contact form 100% functioning

--- contactForm.js
+++ contactForm.js
@@ -5,29 +5,45 @@ const phoneNumber = document.getElementById("phoneNumber");
 const addInfo = document.getElementById("addInfo");
 
 function sendEmail() {
-    const bodyMessage = `Full Name: ${fullName.value}<br>
+    bodyMessage = `Full Name: ${fullName.value}<br>
     Email: ${emailAddress.value}<br> 
     Phone Number: ${phoneNumber.value}<br>
-    Additional Information: ${addInfo.value}<br>`
-    //Need to add Service(s) Requested: 
+    Service(s) Requested: `;
+
+    // Get all checkboxes
+    checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+    // Iterate through checkboxes
+    checkboxes.forEach(checkbox => {
+        // Check if the checkbox is checked
+        if (checkbox.checked) {
+            // Append the checkbox value to the bodyMessage
+            bodyMessage += `${checkbox.name}, `;
+        }
+    });
+
+    // Remove the trailing comma and add a line break
+    bodyMessage = bodyMessage.replace(/,\s*$/, "<br>");
+
+    //Append the additional information value to the bodyMessage
+    bodyMessage += `Additional Information: ${addInfo.value}<br>`
 
     Email.send({
         SecureToken: "f7666d63-15b8-4bf2-ac60-d1a3d8418244",
-        To : 'staceyle2001@gmail.com',
-        From : "staceyle2001@gmail.com",
-        Subject : "Stacey Le Photography - New Contact!",
-        Body : bodyMessage
+        To: 'staceyle2001@gmail.com',
+        From: "staceyle2001@gmail.com",
+        Subject: "Stacey Le Photography - New Contact!",
+        Body: bodyMessage
     }).then(
-      message => {
-        if (message == "OK")
-        {
-            Swal.fire({
-                title: "Success!",
-                text: "Message sent successfully!",
-                icon: "success"
-              });
+        message => {
+            if (message == "OK") {
+                Swal.fire({
+                    title: "Success!",
+                    text: "Message sent successfully!",
+                    icon: "success"
+                });
+            }
         }
-      }
     );
 }
 
