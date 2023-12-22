13e16f2339262f6dd24640f9d2a276abfcb753de
staceyle2001 <staceyle2001@gmail.com>
2023-12-21 18:09:44 -0600

    Latest changes from 12/21/23

---
+++ lightGallery.js
@@ -0,0 +1,8 @@
+// light gallery
+document.addEventListener("DOMContentLoaded", function() 
+{
+    lightGallery(document.querySelector('.lightGallery'),
+    {
+        download: false
+    });
+});
