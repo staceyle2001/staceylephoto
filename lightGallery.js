b5850eb41c002fcde1e5753d10257c3700ef4014
staceyle2001 <staceyle2001@gmail.com>
2023-12-27 18:46:15 -0600

    Finished the 3 portfolios

--- lightGallery.js
+++ lightGallery.js
@@ -6,3 +6,26 @@ document.addEventListener("DOMContentLoaded", function()
         download: false
     });
 });
+
+
+//Expand gallery header
+// Get a reference to the header element
+const header = document.querySelector('.galleryHeader__heading');
+
+// Define a scroll event listener
+window.addEventListener('scroll', () => {
+  // Determine the scroll position
+  const scrollY = window.scrollY;
+
+  // Define the scroll position at which you want the header to expand
+  const scrollThreshold = 50; // Change this value as needed
+
+  // Check if the user has scrolled past the threshold
+  if (scrollY > scrollThreshold) {
+    // Add the "expanded" class to the header element
+    header.classList.add('expanded');
+  } else {
+    // Remove the "expanded" class if the user scrolls back up
+    header.classList.remove('expanded');
+  }
+});
