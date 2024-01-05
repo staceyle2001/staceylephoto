b4b9a3f03c2b487a30394b26b5caa914382adecd
staceyle2001 <staceyle2001@gmail.com>
2024-01-04 19:11:33 -0600

    Updates

--- js/homePage.js
+++ js/homePage.js
@@ -73,27 +73,27 @@ const hideMobileMenu = () =>
 menuLinks.addEventListener('click', hideMobileMenu);
 navLogo.addEventListener('click', hideMobileMenu);
 
-// // Expand welcome header
-// // Get a reference to the header element
-// const header = document.querySelector('.welcome__heading');
+// Expand welcome header
+// Get a reference to the header element
+const header = document.querySelector('.welcome__heading');
 
-// // Define a scroll event listener
-// window.addEventListener('scroll', () => {
-//   // Determine the scroll position
-//   const scrollY = window.scrollY;
+// Define a scroll event listener
+window.addEventListener('scroll', () => {
+  // Determine the scroll position
+  const scrollY = window.scrollY;
 
-//   // Define the scroll position at which you want the header to expand
-//   const scrollThreshold = 50; // Change this value as needed
+  // Define the scroll position at which you want the header to expand
+  const scrollThreshold = 50; // Change this value as needed
 
-//   // Check if the user has scrolled past the threshold
-//   if (scrollY > scrollThreshold) {
-//     // Add the "expanded" class to the header element
-//     header.classList.add('expandWelcomeHeader');
-//   } else {
-//     // Remove the "expanded" class if the user scrolls back up
-//     header.classList.remove('expandWelcomeHeader');
-//   }
-// });
+  // Check if the user has scrolled past the threshold
+  if (scrollY > scrollThreshold) {
+    // Add the "expanded" class to the header element
+    header.classList.add('expandWelcomeHeader');
+  } else {
+    // Remove the "expanded" class if the user scrolls back up
+    header.classList.remove('expandWelcomeHeader');
+  }
+});
 
 // Back to top button 
 // Get a reference to the button element
